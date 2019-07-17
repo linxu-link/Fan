f7adab716cd7bc4c44430f7c4730fe7e824c3824
吴价 <cilingciling2014@outlook.com>
2019-07-17 11:04:39 +0800

    添加更新组件

---
+++ component_update/src/main/java/com/link/component_update/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.link.component_update
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.update_fragment)
+    }
+}
---
+++ component_update/src/main/java/com/link/component_update/UpdateApplication.kt
@@ -0,0 +1,15 @@
+package com.link.component_update
+
+import android.app.Application
+import com.link.librarymodule.BaseApplication
+
+class UpdateApplication:BaseApplication() {
+
+    override fun initModuleData(application: Application) {
+
+    }
+
+    override fun initModuleApp(application: Application) {
+
+    }
+}
