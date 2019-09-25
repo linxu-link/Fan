78d6bfce6383ab04e2bfa4c0ff0f950e8de583f7
linux-link <18911560821@qq.com>
2019-09-25 17:24:52 +0800

    新增login

---
+++ app/src/main/java/com/link/fan/app/login/LoginFragment.kt
@@ -0,0 +1,9 @@
+package com.link.fan.app.login
+
+import androidx.fragment.app.Fragment
+
+class LoginFragment :Fragment(){
+
+
+
+}
---
+++ app/src/main/java/com/link/fan/app/login/LoginViewModel.kt
@@ -0,0 +1,5 @@
+package com.link.fan.app.login
+
+class LoginViewModel{
+
+}
